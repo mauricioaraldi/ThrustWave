e0b148fdc90a7e134e95cb1e868d9ca3aa4bb2f5
Mauricio Araldi <araldi.mauricio@gmail.com>
2024-10-20 21:05:19 +0200

    Finished work

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,15 +9,39 @@ extends RigidBody3D
 var _has_crashed: bool = false
 var _has_completed: bool = false
 
+@onready var explosion_sound: AudioStreamPlayer = $ExplosionSound
+@onready var success_sound: AudioStreamPlayer = $SuccessSound
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles_center: GPUParticles3D = $Model/CenterThrust/BoosterParticles_center
+@onready var booster_particles_right: GPUParticles3D = $Model/RightThrust/BoosterParticles_right
+@onready var booster_particles_left: GPUParticles3D = $Model/LeftThrust/BoosterParticles_left
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
+
+
 func _process(delta: float) -> void:
   if Input.is_action_pressed("boost"):
     apply_central_force(basis.y * delta * thrust)
+  if (Input.is_action_just_pressed("boost")):
+    rocket_audio.play()
+    booster_particles_center.emitting = true;
+  if Input.is_action_just_released("boost"):
+    rocket_audio.stop()
+    booster_particles_center.emitting = false;
 
   if Input.is_action_pressed("rotate_left"):
     apply_torque(Vector3(0, 0, torque * delta))
+  if (Input.is_action_just_pressed("rotate_left")):
+    booster_particles_right.emitting = true;
+  if Input.is_action_just_released("rotate_left"):
+    booster_particles_right.emitting = false;
 
   if Input.is_action_pressed("rotate_right"):
     apply_torque(Vector3(0, 0, -torque * delta))
+  if (Input.is_action_just_pressed("rotate_right")):
+    booster_particles_left.emitting = true;
+  if Input.is_action_just_released("rotate_right"):
+    booster_particles_left.emitting = false;
 
 
 func _on_body_entered(body: Node) -> void:
@@ -31,16 +55,26 @@ func _on_body_entered(body: Node) -> void:
 func crash_sequence() -> void:
   _has_crashed = true
   set_process(false)
+  explosion_sound.play()
+  explosion_particles.emitting = true;
+  booster_particles_center.emitting = false;
+  booster_particles_left.emitting = false;
+  booster_particles_right.emitting = false;
 
   var tween = create_tween()
-  tween.tween_interval(1.0)
+  tween.tween_interval(2.0)
   tween.tween_callback(get_tree().reload_current_scene)
 
 
 func complete_level(next_level_file: String) -> void:
-  _has_completed = true;
+  _has_completed = true
   set_process(false)
+  success_sound.play()
+  success_particles.emitting = true;
+  booster_particles_center.emitting = false;
+  booster_particles_left.emitting = false;
+  booster_particles_right.emitting = false;
 
   var tween = create_tween()
-  tween.tween_interval(1.0)
+  tween.tween_interval(1.5)
   tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
